a6c5053cd8affbe998524f7d17e476bf7a692128
IvanBorozdin <120761206+IvanBorozdin@users.noreply.github.com>
2023-01-07 11:51:41 +0700

    ДЗ 6

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задания 1");
+
+        for(int i = 1; i <= 10; i = i + 1){
+            System.out.println(i);
+        }
+
+    {
+        System.out.println("Задагия 2");
+        for(int i = 10; i <= 10 && i>=1; i = i -1){
+            System.out.println(i);
+        }
+    }
+        {
+            System.out.println("Задания 3");
+            for(int i = 0; i <= 17; i = i + 2)
+            {
+                System.out.println(i);
+            }
+        }
+        {
+            System.out.println("Задания 4");
+
+            for(int i = 10; i <= 10 && i>= -10; i = i -1){
+                System.out.println(i);
+            }
+
+        }
+        {
+            System.out.println("Задания 5");
+        }
+        for (int i = 1904;i <2096;i = i +4){
+            System.out.println(i+" год является високосным");
+        }
+        {
+            System.out.println("Задания 6");
+        }
+        for (int i =7 ;i < 100 ;i = i + 7){
+            System.out.println(i);
+        }
+        {
+            System.out.println("Задания 7");
+        }
+        for (int i=1; i < 600; i = i * 2){
+            System.out.println(i);
+        }
+        {
+            System.out.println("Задания 8");
+
+        int salary =29000;
+        int total=0;
+        for (int i=0; i<=12;i++){
+            total = total+ salary;
+            System.out.println("Месяц "+i+" сумма накоплений равна "+total+" рублей" );
+
+        }}
+        {
+            System.out.println("Задания 9");
+
+        int salary = 29000;
+        int total = 0;
+        for (int i=0; i<=12;i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }}
+        {
+            System.out.println("Задания 10");
+        }
+        int a=2;
+        for (int i=1;i<=10;i++){
+            System.out.println(a+"*"+i+"="+(i*a));
+        }
+
+        }}
+
